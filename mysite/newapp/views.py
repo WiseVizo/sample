b19e44dc69c047bc6c92cee5ee4ee621d081f549
WiseVizo <mecoc1011@gmail.com>
2024-04-20 10:33:05 +0530

    search and pagination implemented

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .models import MovieData
+from django.core.paginator import Paginator
 # Create your views here.
+
+def movie_list(request):
+    movie_data = MovieData.objects.all()
+    if request.method == 'POST':
+        search_query = request.POST.get('search')
+        if search_query:
+            # Filter the movie_data queryset based on the search query
+            movie_data = movie_data.filter(name__icontains=search_query)
+    paginator = Paginator(movie_data, 3)
+    page = request.GET.get("page")
+    movie_data = paginator.get_page(page)
+    context = {
+        'page_obj': movie_data,
+    }
+    return render(request, "newapp/movie_list.html", context)
+
+   
